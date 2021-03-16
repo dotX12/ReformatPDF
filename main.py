80e18f055d722ba678c3e0826a80f85aadf710cc
dotX12 <hash@shitposting.team>
2021-03-16 19:21:35 +0300

    Main file for work

---
+++ main.py
@@ -0,0 +1,6 @@
+from ReformatPDF import ReformatPDF
+
+pdf = ReformatPDF()
+pdf.translate(pdf_name='PDF/Информация о ходе приёма заявлений в ДГТУ на бюджетной основе. Очная форма обучения.pdf',
+              new_json_name='JSON/Информация о ходе приёма заявлений в ДГТУ на бюджетной основе. Очная форма обучения.json',
+              delete_temp=0)
